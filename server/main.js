d9360adf37d574469094dee5b0d74d68199159fc
josh <josh@coinapult.com>
2014-04-13 07:03:09 -0500

    EUREKA! Websocket subscriptions are working perfectly.
    Upon startup, the server subscribes to all users with addresses and then whenever
    it receives a transaction, it updates the balance in the database.
    
    This is the stable base to build on.
    
    I think the next step is to build in the qr code generation, that will give
    us the pieces for the widget. Then we switch over to bootstrap, etc

--- server/main.js
+++ server/main.js
@@ -2,6 +2,98 @@
  * Created by josh on 4/13/14.
  */
 
+
+
+function subscribeToAll(ws) {
+    var addresses = Meteor.users.find({'profile.bitcoin_address': {$exists: true}}, {'profile.bitcoin_address': 1, _id: 0});
+    addresses.forEach(function (each) {
+        var requestObj = {"op": "addr_sub", "addr": each.profile.bitcoin_address};
+        var request = JSON.stringify(requestObj);
+        ws.sendUTF(request);
+        console.log(request);
+        var testRequest = {"op": "ping_tx"};
+        ws.send(JSON.stringify(testRequest));
+    })
+}
+
+
+function handleInputs(tx) {
+    try {
+        tx.x.inputs.forEach(function (entry) {
+            var input = Object();
+            input.address = entry.prev_out.addr;
+            input.value = entry.prev_out.value;
+            Meteor.users.update(
+                {'profile.bitcoin_address': input.address},
+                {$inc: {'profile.bitcoin_balance': input.value}}, upsert = false);
+        });
+    }
+    catch (e) {
+        console.log(e.stack);
+    }
+}
+
+
+function handleOutputs(tx) {
+    try {
+        tx.x.out.forEach(function (output) {
+            var myOut = Object();
+            myOut.address = output.addr;
+            myOut.value = output.value;
+            Meteor.users.update(
+                {'profile.bitcoin_address': myOut.address},
+                {$inc: {'profile.bitcoin_balance': myOut.value}}, upsert = false);
+        })
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+
+var incomingTransaction = function (tx) {
+    console.log(tx);
+    handleInputs(tx);
+    handleOutputs(tx);
+};
+
+function connectionErrorHandler(error) {
+    console.log("Connection Error: " + error.toString());
+}
+
+function connectionCloseHandler() {
+    console.log('echo-protocol Connection Closed');
+}
+
+function connectionMessageHandler(message) {
+    jsonMessage = JSON.parse(message.utf8Data);
+    if (jsonMessage.op == 'utx') {
+        console.log('first test passed');
+        try {
+            incomingTransaction(jsonMessage);
+        }
+        catch (e) {
+            console.log(e.stack)
+        }
+    }
+}
+
+function connectFailedHandler(error) {
+    console.log('Connect Error: ' + error.toString());
+}
+
+wrappedConnectionErrorHandler = Meteor.bindEnvironment(connectionErrorHandler);
+wrappedConnectionCloseHandler = Meteor.bindEnvironment(connectionCloseHandler);
+wrappedConnectionMessageHandler = Meteor.bindEnvironment(connectionMessageHandler);
+
+function connectSuccessHandler(connection) {
+    console.log('WebSocket client connected');
+    subscribeToAll(connection);
+    connection.on('error', wrappedConnectionErrorHandler);
+    connection.on('close', wrappedConnectionCloseHandler);
+    connection.on('message', wrappedConnectionMessageHandler);
+}
+
 var startWebSocket = function () {
     var WebSocketClient = Meteor.require('websocket').client;
     var client = new WebSocketClient();
@@ -11,7 +103,7 @@ var startWebSocket = function () {
 };
 
 Meteor.startup(function () {
-        Meteor.users.find().forEach(function (each) {
+    Meteor.users.find().forEach(function (each) {
         console.log("Bitcoin Address: " + each.profile.bitcoin_address);
         var url = "https://blockchain.info/q/addressbalance/" + String(each.profile.bitcoin_address) + "?confirmations=0";
         var result = HTTP.get(url, 3000);
@@ -23,5 +115,6 @@ Meteor.startup(function () {
                 {$set: {'profile.bitcoin_balance': Number(response)}});
         }
     });
-    var WebSocketClient = Meteor.require('websocket').client;
+    startWebSocket();
+
 });
