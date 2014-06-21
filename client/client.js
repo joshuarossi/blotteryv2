1fa4b10d6bd8936c8eea516fada4a30547103230
josh <josh@coinapult.com>
2014-06-21 16:54:18 -0500

    added iron-router routes, so we can set up routes for the profile page as well as the iframe plugin

--- client/client.js
+++ client/client.js
@@ -2,18 +2,14 @@
  * Created by josh on 4/13/14.
  */
 
-Template.main.balances = function () {
+Template.balances.balances = function () {
     return Meteor.users.find();
 };
 
-Template.main.events({
+Template.balances.events({
     'click input': function () {
         // template data, if any, is available in 'this'
         if (typeof console !== 'undefined')
             console.log("You pressed the button");
     }
-});
-
-Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
---
+++ routes.js
@@ -0,0 +1,8 @@
+/**
+ * Created by josh on 6/21/14.
+ */
+Router.map(function () {
+    this.route('home', {path: '/'});
+    this.route('balances', {path: '/balances'});
+    this.route('profile', {path: '/profile'});
+});
