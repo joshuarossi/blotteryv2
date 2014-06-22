ce96a060ed36a17e154e8acb1ee35a93f15a4734
josh <josh@coinapult.com>
2014-06-21 20:42:59 -0500

    finished the form, it now updates correctly. I will hide more fields and adjust later

--- client/client.js
+++ client/client.js
@@ -14,6 +14,26 @@ UI.registerHelper("Users", Meteor.users);
 Template.signup_form.editingDoc = function () {
     return Meteor.user();
 };
+Template.signup_form.rendered = function () {
+    user = Meteor.user();
+    console.log(user);
+    satoshis = user.profile.bitcoin_balance;
+    console.log(satoshis);
+    bitcoin = satoshis / 1e8;
+    console.log(bitcoin);
+    return bitcoin
+};
+
+Template.signup_form.balance = function () {
+    user = Meteor.user();
+    console.log(user);
+    satoshis = user.profile.bitcoin_balance;
+    console.log(satoshis);
+    bitcoin = satoshis/1e8;
+    console.log(bitcoin);
+    return bitcoin
+};
+
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
