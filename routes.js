5d59370af4b68e06a3048d4217f1b854d8e2ddef
josh <josh@coinapult.com>
2014-06-22 01:32:28 -0500

    added in header and footer to a master template and added that master template to the home route

--- routes.js
+++ routes.js
@@ -3,7 +3,15 @@
  */
 
 Router.map(function () {
-    this.route('home', {path: '/'});
+    this.route('home', {
+        path: '/',
+        template: 'home',
+        layoutTemplate: 'layout',
+        yieldTemplates: {
+            'header': {to: 'header'},
+            'footer': {to: 'footer'}
+        }
+    });
     this.route('balances', {path: '/balances'});
     this.route('signup_form', {path: '/signup'});
     this.route('about');
