81c11b7f3920ac7c7bcd75d6f3825238085668f7
josh <josh@coinapult.com>
2014-06-22 01:22:46 -0500

    placeholders for header, footer and notfound page

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 /**
  * Created by josh on 6/21/14.
  */
+
 Router.map(function () {
     this.route('home', {path: '/'});
     this.route('balances', {path: '/balances'});
-    this.route('profile', {path: '/profile'});
     this.route('signup_form', {path: '/signup'});
     this.route('about');
     this.route('plugin', {
